658b402724c6ac9f07da1a7e1465e4f0a8112137
chris <acomputerdog@gmail.com>
2019-02-10 09:26:22 -0500

    Remove old test cases

--- src/test/java/net/acomputerdog/bigwarps/TestBiMap.java
+++ src/test/java/net/acomputerdog/bigwarps/TestBiMap.java
@@ -45,41 +45,4 @@ public class TestBiMap {
         Assert.assertFalse(map.containsA("three"));
         Assert.assertFalse(map.containsB(3));
     }
-
-    @Test
-    public void testGenericGet() {
-        BiMap<String, Integer> map = new BiMap<>();
-        map.put("one", 1);
-        map.put("two", 2);
-        map.put("three", 3);
-
-        Integer one = 1;
-        Integer two = 2;
-        Integer three = 3;
-
-        Assert.assertEquals(one, map.get("one"));
-        Assert.assertEquals("one", map.get(one));
-        Assert.assertEquals(two, map.get("two"));
-        Assert.assertEquals("two", map.get(two));
-        Assert.assertEquals(three, map.get("three"));
-        Assert.assertEquals("three", map.get(three));
-    }
-
-    @Test
-    public void testGenericRemove() {
-        BiMap<String, Integer> map = new BiMap<>();
-        map.put("one", 1);
-        map.put("two", 2);
-        map.put("three", 3);
-
-        map.remove("one");
-        map.remove(3);
-
-        Assert.assertFalse(map.containsA("one"));
-        Assert.assertFalse(map.containsB(1));
-        Assert.assertTrue(map.containsA("two"));
-        Assert.assertTrue(map.containsB(2));
-        Assert.assertFalse(map.containsA("three"));
-        Assert.assertFalse(map.containsB(3));
-    }
 }
